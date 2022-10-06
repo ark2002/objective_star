dc07870b8ebe2a418110900c80582a7e8cad89c2
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-06 23:42:55 +0530

    feat : added to App

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
-import './App.css';
-import { Main } from './frontend/components';
+import { Toaster } from "react-hot-toast";
+import "./App.css";
+import { Main } from "./frontend/components";
 
 function App() {
   return (
     <div className="App">
       <Main />
+      <Toaster
+        position="bottom-center"
+        toastOptions={{ duration: 2000, className: "toast" }}
+      />
     </div>
   );
 }
