1153294d5ab33bb176bc773ba454fb2b1b693365
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-06 23:44:19 +0530

    feat : updated celebrityReducer

--- src/frontend/reducer/celebrityReducer.js
+++ src/frontend/reducer/celebrityReducer.js
@@ -1,8 +1,19 @@
+import moment from "moment";
+
 export const celebrityReducer = (state, action) => {
   const { type, payload } = action;
   switch (type) {
     case "initialSet":
-      return { ...state, celebrityList: payload };
+      return {
+        ...state,
+        celebrityList: payload.map((user) => {
+          return {
+            ...user,
+            age: moment().diff(user.dob, "years"),
+            fullName: `${user.first} ${user.last}`,
+          };
+        }),
+      };
     case "deleteCelebrity":
       return { ...state, deleteList: [...state.deleteList, payload] };
     case "filter":
@@ -10,6 +21,13 @@ export const celebrityReducer = (state, action) => {
         ...state,
         celebrityList: state.celebrityList.filter(
           (user) => state.deleteList.indexOf(user) === -1
+        ),
+      };
+    case "edit":
+      return {
+        ...state,
+        celebrityList: state.celebrityList.map((user) =>
+          user.id === payload.id ? { ...payload } : user
         ),
       };
     default:
