c95e57db597947cc051441b636515ae79db58532
Aryak Lahane <67817330+ark2002@users.noreply.github.com>
2022-10-06 23:45:32 +0530

    update: userItemExpanded

--- src/frontend/components/UserItemExpanded/UserItemExpanded.js
+++ src/frontend/components/UserItemExpanded/UserItemExpanded.js
@@ -1,54 +1,71 @@
-import moment from "moment";
-import { useCelebrity } from "../../context/CelebrityContext";
+import { useState } from "react";
+import toast from "react-hot-toast";
+import { DeleteModal } from "../DeleteModal/DeleteModal";
+import { EditCard } from "../EditCard/EditCard";
 import "./UserItemExpanded.css";
 
-const UserItemExpanded = ({ star, setIsOpen }) => {
-  const { first, last, picture, gender, dob, country, description } = star;
-  const { dispatchList } = useCelebrity();
+const UserItemExpanded = ({ star, setIsOpen, edit, setEdit }) => {
+  const [modal, setModal] = useState(false);
+  const { fullName, picture, gender, age, country, description } = star;
 
   return (
-    <div className="flex__column-center user-expanded__container">
-      <div
-        className="flex__row-center user__top"
-        onClick={() => setIsOpen(null)}
-      >
-        <img className="user__img" src={picture} alt={first} />
-        <div className="user__name flex__row-center">
-          <p className="name__first">{first}</p>
-          <p className="name__last">{last}</p>
+    <>
+      {edit ? (
+        <div className="edit__restriction">
+          <EditCard setEdit={setEdit} star={star} />
         </div>
-        <span className="material-icons collapsed__icon">expand_less</span>
-      </div>
-      <div className="flex__row-center user__middle">
-        <div className="flex__column">
-          <p className="user__label">Age</p>
-          <p className="user__details">{moment().diff(dob, "years")} years</p>
+      ) : (
+        <div className="flex__column-center user-expanded__container">
+          <div
+            className="flex__row-center user__top"
+            onClick={() => setIsOpen(null)}
+          >
+            <img className="user__img" src={picture} alt={fullName} />
+            <p className="user__name">{fullName}</p>
+            <span className="material-icons collapsed__icon">expand_less</span>
+          </div>
+          <div className="flex__row-center user__middle">
+            <div className="flex__column">
+              <p className="user__label">Age</p>
+              <p className="user__details">{age} years</p>
+            </div>
+            <div className="flex__column">
+              <p className="user__label">Gender</p>
+              <p className="user__details">{gender}</p>
+            </div>
+            <div className="flex__column">
+              <p className="user__label">Country</p>
+              <p className="user__details">{country}</p>
+            </div>
+          </div>
+          <div className="flex__column user__bottom">
+            <p className="user__label">Description</p>
+            <p className="user__description">{description}</p>
+          </div>
+          <div className="flex__row-center user__options">
+            <span
+              className="material-icons delete__icon"
+              onClick={() => {
+                setModal(true);
+              }}
+            >
+              delete
+            </span>
+            <span
+              className="material-icons edit__icon"
+              onClick={() =>
+                age > 17
+                  ? setEdit(true)
+                  : toast.error("User is not an adult! Can't Edit.")
+              }
+            >
+              edit
+            </span>
+          </div>
         </div>
-        <div className="flex__column">
-          <p className="user__label">Gender</p>
-          <p className="user__details">{gender}</p>
-        </div>
-        <div className="flex__column">
-          <p className="user__label">Country</p>
-          <p className="user__details">{country}</p>
-        </div>
-      </div>
-      <div className="flex__column user__bottom">
-        <p className="user__label">Description</p>
-        <p className="user__description">{description}</p>
-      </div>
-      <div className="flex__row-center user__options">
-        <span
-          className="material-icons delete__icon"
-          onClick={() =>
-            dispatchList({ type: "deleteCelebrity", payload: star })
-          }
-        >
-          delete
-        </span>
-        <span className="material-icons edit__icon">edit</span>
-      </div>
-    </div>
+      )}
+      <DeleteModal modal={modal} setModal={setModal} star={star} />
+    </>
   );
 };
 
